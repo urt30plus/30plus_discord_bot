1d64701d5118cac381d36008856606257e16f4ce
urt30plus <urt30plus@gmail.com>
2023-03-11 09:00:42 -0800

    split ci jobs into linting with pre-commit and checks using mypy/unittest

---
+++ run_checks.sh
@@ -0,0 +1,15 @@
+set -xe
+
+mypy --version
+mypy
+
+export BOT_USER=30+Bot#TEST
+export BOT_SERVER_NAME=30+ Urban Test
+export BOT_TOKEN=sekret
+export MAPCYCLE_EMBED_TITLE=Map Cycle
+export CHANNEL_NAME_MAPCYCLE=test-mapcycle
+export MAPCYCLE_FILE=./tests/data/mapcycle.txt
+export CURRENT_MAP_EMBED_TITLE=Current Map
+
+python -V
+python -m unittest
